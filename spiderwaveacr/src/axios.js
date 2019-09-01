4d8df79f7119a18e23d38339f7b8b9314cfb763a
ambika <abhi.fullstackdeveloper@gmail.com>
2019-09-01 12:02:42 +0530

    new changes

--- spiderwaveacr/src/axios.js
+++ spiderwaveacr/src/axios.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3300',
-  //baseURL: 'http://3.83.247.178:3300',
+  //baseURL: 'http://localhost:3300',
+  baseURL: 'http://3.83.247.178:3300',
   // timeout: 1000,
   //headers: {'Authorization': 'foobar'}
 });
