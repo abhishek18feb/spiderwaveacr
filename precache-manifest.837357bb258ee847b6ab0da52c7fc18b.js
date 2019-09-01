85fdd987afe164fb7fe86e43bd55a044f7dd07de
ambika <abhi.fullstackdeveloper@gmail.com>
2019-09-01 14:36:37 +0530

    new changes

---
+++ precache-manifest.837357bb258ee847b6ab0da52c7fc18b.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2e2a055aceb2d122149c71772d681e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cce684d29a9d780c280",
+    "url": "/static/css/10.f7d25d02.chunk.css"
+  },
+  {
+    "revision": "1b10315efce0cd250b4e",
+    "url": "/static/css/11.4dda2681.chunk.css"
+  },
+  {
+    "revision": "88dac31622ced53d366c",
+    "url": "/static/css/12.c41d6b32.chunk.css"
+  },
+  {
+    "revision": "18c7684b1da9ce44ae1e",
+    "url": "/static/css/4.7441f375.chunk.css"
+  },
+  {
+    "revision": "a86182ca049596833844",
+    "url": "/static/css/5.ea51a409.chunk.css"
+  },
+  {
+    "revision": "fa9995a4ebff8af71637",
+    "url": "/static/css/6.7441f375.chunk.css"
+  },
+  {
+    "revision": "f2fd72df5f489c8446b1",
+    "url": "/static/css/7.ea51a409.chunk.css"
+  },
+  {
+    "revision": "0bbc50703abcc51c98d6",
+    "url": "/static/css/8.4dda2681.chunk.css"
+  },
+  {
+    "revision": "1d56ca84de633dfa46f5",
+    "url": "/static/css/9.b0bef949.chunk.css"
+  },
+  {
+    "revision": "84800369cf714372987d",
+    "url": "/static/css/main.2f2758be.chunk.css"
+  },
+  {
+    "revision": "b264d79e6a039f379590",
+    "url": "/static/js/0.8ece5f97.chunk.js"
+  },
+  {
+    "revision": "9cce684d29a9d780c280",
+    "url": "/static/js/10.e0411b55.chunk.js"
+  },
+  {
+    "revision": "1b10315efce0cd250b4e",
+    "url": "/static/js/11.ccd94f5b.chunk.js"
+  },
+  {
+    "revision": "88dac31622ced53d366c",
+    "url": "/static/js/12.6c8905cc.chunk.js"
+  },
+  {
+    "revision": "0e57a50941b7fc329c9f",
+    "url": "/static/js/13.938d1c99.chunk.js"
+  },
+  {
+    "revision": "7e7cb07057bdef248172",
+    "url": "/static/js/3.dde6a7e3.chunk.js"
+  },
+  {
+    "revision": "18c7684b1da9ce44ae1e",
+    "url": "/static/js/4.fa6fbb37.chunk.js"
+  },
+  {
+    "revision": "a86182ca049596833844",
+    "url": "/static/js/5.ce30bb7e.chunk.js"
+  },
+  {
+    "revision": "fa9995a4ebff8af71637",
+    "url": "/static/js/6.4c30dac1.chunk.js"
+  },
+  {
+    "revision": "f2fd72df5f489c8446b1",
+    "url": "/static/js/7.d92637c5.chunk.js"
+  },
+  {
+    "revision": "0bbc50703abcc51c98d6",
+    "url": "/static/js/8.1e064683.chunk.js"
+  },
+  {
+    "revision": "1d56ca84de633dfa46f5",
+    "url": "/static/js/9.3f3459b2.chunk.js"
+  },
+  {
+    "revision": "84800369cf714372987d",
+    "url": "/static/js/main.7a1f3c06.chunk.js"
+  },
+  {
+    "revision": "bcb896e96339c7dcdb71",
+    "url": "/static/js/runtime~main.57424e48.js"
+  },
+  {
+    "revision": "e456243191ad4e0c1eaa22fa37990761",
+    "url": "/static/media/nature-natural-beauty.e4562431.jpg"
+  }
+]);
