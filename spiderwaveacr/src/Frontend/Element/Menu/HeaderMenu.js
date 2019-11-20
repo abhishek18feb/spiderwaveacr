0d32886e6f185b24733fb32d8d8eeb84179fccae
ambika <abhi.fullstackdeveloper@gmail.com>
2019-11-20 08:34:21 +0530

    completed popup section

--- spiderwaveacr/src/Frontend/Element/Menu/HeaderMenu.js
+++ spiderwaveacr/src/Frontend/Element/Menu/HeaderMenu.js
@@ -3,25 +3,24 @@ import {Link, NavLink} from 'react-router-dom';
 import Login from '../Popup/Login/Login';
 
 const HeaderMenu = props => {
-	const [showLoginPopup, displayPopup]=useState(false);
-	const setShowLoginPopup = ()=>{
-		displayPopup(true)
-	}
+	const [showLoginPopup, displayPopup]=useState("none");
+
     return (
         <React.Fragment>
-			<Login show={showLoginPopup} />
+			
             <div className="w3-top">
 				<div className="w3-bar w3-white w3-wide w3-padding w3-card">
 					<a href="#home" className="w3-bar-item w3-button"><b>Spider</b> Waveacr</a>
 					
 					<div className="w3-right w3-hide-small">
-						<a href="#projects" className="w3-bar-item w3-button" onClick={setShowLoginPopup}>Login/Signup</a>
+						<a href="#projects" className="w3-bar-item w3-button" onClick={()=>displayPopup("block")}>Login/Signup</a>
 						<a href="#contact" className="w3-bar-item w3-button">Contact Us</a>
                         <a href="#services" className="w3-bar-item w3-button">Services</a>
                         <a href="#address" className="w3-bar-item w3-button">Address</a>
 					</div>
 				</div>
 			</div>
+			<Login show={showLoginPopup} displayPopup={displayPopup} />
         </React.Fragment>
     )
 }
--- spiderwaveacr/src/Frontend/Element/Popup/Login/Login.js
+++ spiderwaveacr/src/Frontend/Element/Popup/Login/Login.js
@@ -1,24 +1,34 @@
 import React from 'react';
 
 const Login =props=>{
-    console.log(props.show)
-    const [displayPopup, setDiplayPopup] = React.useState(props.show);
-    console.log(displayPopup)
-    // const setDiplayPopupHandler = ()=>{
-    //     setDiplayPopup(false);
-    // }
+    
+    const {show, displayPopup} = props;
     
     return (
-        <React.Fragment>
-            <div id="id01" className="w3-modal" style={{display: displayPopup?"block":"none"}}>
+        <React.Fragment> 
+            <div id="id01" className="w3-modal" style={{display: show}}>
                 <div className="w3-modal-content w3-card-4">
                 <header className="w3-container w3-teal"> 
-                    <span  className="w3-button w3-display-topright">×</span>
+                    <span  className="w3-button w3-display-topright" 
+                        onClick={()=>displayPopup(false)}
+                    >×</span>
                     <h2>Modal Header</h2>
                 </header>
                 <div className="w3-container">
-                    <p>Some text..</p>
-                    <p>Some text..</p>
+                    <div className="form-popup" id="myForm">
+                        <form action="/action_page.php" class="form-container">
+                            <h1>Login</h1>
+
+                            <label for="email"><b>Email</b></label>
+                            <input type="text" placeholder="Enter Email" name="email" required />
+
+                            <label for="psw"><b>Password</b></label>
+                            <input type="password" placeholder="Enter Password" name="psw" required />
+
+                            <button type="submit" class="btn">Login</button>
+                            
+                        </form>
+                    </div>
                 </div>
                 <footer className="w3-container w3-teal">
                     <p>Modal Footer</p>
